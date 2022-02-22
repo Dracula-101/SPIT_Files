9a71c16e58f334b697346c94a2c2cab436babcd6
Dracula-101 <66874986+Dracula-101@users.noreply.github.com>
2022-02-22 23:41:42 +0530

    DAA changes

--- DAA/EXP 3/Driver.java
+++ DAA/EXP 3/Driver.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // Arraylist of Integers
         ArrayList<Integer> list = new ArrayList<Integer>();
@@ -29,44 +30,79 @@ public class Driver {
         }
         System.out.print("\n1.Best Case\n2.Worst Case\n3.Average Case\n4.Manual Choice\nEnter your choice: ");
         int newChoice = input.nextInt();
-        int arrLen = list.size();
-        array = new int[arrLen];
+        int listSize = list.size();
+        array = new int[listSize];
         switch (newChoice) {
             case 1:
-                for (int i = 0; i < arrLen; i++) {
+                for (int i = 0; i < listSize; i++) {
                     array[i] = list.get(i);
                 }
                 break;
             case 2:
-                for (int i = arrLen - 1; i >= 0; i--) {
-                    array[(arrLen - 1) - i] = list.get(i);
+                for (int i = listSize - 1; i >= 0; i--) {
+                    array[(listSize - 1) - i] = list.get(i);
                 }
                 break;
             case 3:
                 Collections.shuffle(list);
-                for (int i = 0; i < arrLen; i++) {
+                for (int i = 0; i < listSize; i++) {
                     array[i] = list.get(i);
                 }
                 break;
             case 4:
-                System.out.print("Enter the size of the array: ");
-                int size = input.nextInt();
-                list.clear();
-                System.out.print("Enter the elements of the array(with space): ");
-                for (int i = 0; i < size; i++) {
-                    list.add(input.nextInt());
+                if (choice == 1) {
+                    System.out.print("\nAre u sure u want to enter the array manually?(y/n): ");
+                    String choice1 = input.next().toLowerCase();
+                    if (choice1.equals("y")) {
+                        System.out.print("Enter the size of the array: ");
+                        int size = input.nextInt();
+                        list.clear();
+                        System.out.print("Enter the elements of the array(with space): ");
+                        for (int i = 0; i < size; i++) {
+                            list.add(input.nextInt());
+                        }
+                    }
                 }
                 break;
             default:
                 System.out.println("Invalid choice");
                 break;
         }
-        System.out.print("\nIteration Buuble Sort\n");
+        int arrLen = array.length;
+        // Array segregation
+        switch (arrLen) {
+            case 0:
+                input.close();
+                throw new Exception("Array is empty");
+            case 1:
+                input.close();
+                throw new Exception("Array has only one element");
+            default:
+                System.out.print("\nArray: " + Arrays.toString(array));
+                break;
+        }
+        System.out
+                .print("\n***********************************************\n");
+        System.out.print("\nBubble Sort\n");
+        // Bubble Sort
+        System.out.print("\n1.Iteration Buuble Sort\n2.Recursive Buuble Sort\n3.Bubble Sort (without for loops)   : ");
+        int choice2 = input.nextInt();
         // Create bubble sort object
         BubbleSort bs = new BubbleSort();
         // Call bubble sort function
-        bs.bubbleSortIteration(array);
+        System.out.print("\nBefore sorting: " + Arrays.toString(array));
+        System.out.print("\n---------------------------------------------\n");
+        if (choice2 == 1) {
+            bs.bubbleSortIteration(array);
+        } else if (choice2 == 2) {
+            bs.bubbleSortRec(array, listSize);
+            System.out.print("\nTotal Swaps: " + bs.getSwapCount());
+        } else {
+            bs.bubbleSort(array);
+        }
 
+        System.out.print("\n---------------------------------------------\n");
+        System.out.print("\nFinal Array: " + Arrays.toString(array) + " \n");
         // input closing method
         input.close();
     }
--- DAA/EXP 3/bubblesort.java
+++ DAA/EXP 3/bubblesort.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
+    int counter = 0;
+    int swapNum = 0;
+    static int maxPos = 0;
+    String swaps = "Swaps: |";
+
     // swap function
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
@@ -13,22 +18,102 @@ public class BubbleSort {
     public void bubbleSortIteration(int arr[]) {
         int n = arr.length;
         System.out.printf("Iteration\tSwap\t\tArray\n");
-        int counter = 0;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 ++counter;
                 if (arr[j] > arr[j + 1]) {
                     // swap arr[j+1] and arr[j]
-                    System.out.printf("%d\t\t%d<->%d\t", counter, arr[j], arr[j + 1]);
-                    System.out.print(Arrays.toString(arr) + "\n");
+                    System.out.printf("%d\t\t%d<->%d\t\t%s\n", counter, arr[j], arr[j + 1], printArr(arr));
+
+                    // swappging the elements
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 } else {
-                    System.out.printf("%d\t\t----\t", counter);
-                    System.out.print(Arrays.toString(arr) + "\n");
+                    System.out.printf("%d\t\t----\t\t%s\n", counter, printArr(arr));
                 }
             }
         }
     }
+
+    public void bubbleSortRec(int[] arr, int n) {
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                ++swapNum;
+                // swap arr[i+1] and arr[i]
+                swaps += arr[i] + "<->" + arr[i + 1] + " | ";
+                // swappging the elements
+                swap(arr, i, i + 1);
+            }
+        }
+
+        if (n - 1 > 1) {
+            System.out.print("\n\n" + swaps);
+            System.out.print(
+                    "\nUnsorted + Sorted => " + printArr(arr, 0, n - 1) + " " + printArr(arr, n - 1, arr.length)
+                            + "\n");
+            swaps = "Swaps: |";
+            bubbleSortRec(arr, n - 1);
+        }
+    }
+
+    public int getSwapCount() {
+        return swapNum;
+    }
+
+    public int[] bubbleSort(int i, int[] arr) {
+        if (arr[i] > arr[i + 1] && (i + 1) < arr.length - 1) {
+            swap(arr, i, i + 1);
+            maxPos++;
+        }
+        if (i < arr.length - 1) {
+            System.out.print("\n-----------------------------------------------------");
+            System.out.println("\nMax Places " + arr[i] + " is shifted " + maxPos + " times");
+            System.out.println("\nArray: " + printArr(arr));
+            maxPos = 0;
+            bubbleSort(i + 1, arr);
+        }
+        return arr;
+    }
+
+    public void bubbleSort(int arr[], int n) {
+        // Base case
+        if (n == 1)
+            return;
+
+        if (arr[n - 1] > arr[n - 2]) {
+            swap(arr, n - 1, n - 2);
+            maxPos++;
+            bubbleSort(arr, n - 1);
+        }
+
+        bubbleSort(arr, n - 1);
+    }
+
+    public int[] bubbleSort(int[] arr) {
+        return bubbleSort(0, arr);
+    }
+
+    public void duplicateRemove(int arr[]) {
+
+    }
+
+    public String printArr(int arr[]) {
+        // printing the array
+        if (arr.length > 10)
+            return " ... ";
+        else
+            return Arrays.toString(arr);
+    }
+
+    public String printArr(int[] arr, int start, int end) {
+        // printing the array
+        if (arr.length > 10)
+            return " ... ";
+        else
+            return Arrays.toString(Arrays.copyOfRange(arr, start, end));
+    }
+    // They need the array size case
+
 }
