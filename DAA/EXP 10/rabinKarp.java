cff9982c3adb390e7f93d65f2f4ef7124070f5a3
Dracula-101 <66874986+Dracula-101@users.noreply.github.com>
2022-04-29 23:00:02 +0530

    Added New Files

--- DAA/EXP 10/rabinKarp.java
+++ DAA/EXP 10/rabinKarp.java
@@ -4,48 +4,45 @@ import java.util.Scanner;
 public class rabinKarp {
     static ArrayList<Integer> locations = new ArrayList<Integer>();
 
-    // d is the number of characters in the input alphabet public final static int
+    // d is the number of characters in the input alphabet
     static void search(String pat, String txt, int q) {
-        int d = 256;
-        int M = pat.length();
-        int N = txt.length();
+        int alphabets = 256;
+        int patternLen = pat.length();
+        int textLen = txt.length();
         int i, j;
-        int p = 0; // hash value for pattern
-        int t = 0; // hash value for txt
+        int patHash = 0; // hash value for pattern
+        int textHash = 0; // hash value for txt
         int h = 1;
         // The value of h would be "pow(d, M-1)%q"
-        for (i = 0; i < M - 1; i++)
-            h = (h * d) % q;
+        for (i = 0; i < patternLen - 1; i++)
+            h = (h * alphabets) % q;
         // Calculate the hash value of pattern and first
         // window of text
-        for (i = 0; i < M; i++) {
-            p = (d * p + pat.charAt(i)) % q;
-            t = (d * t + txt.charAt(i)) % q;
+        for (i = 0; i < patternLen; i++) {
+            patHash = (alphabets * patHash + pat.charAt(i)) % q;
+            textHash = (alphabets * textHash + txt.charAt(i)) % q;
         }
         // Slide the pattern over text one by one
-        for (i = 0; i <= N - M; i++) {
+        for (i = 0; i <= textLen - patternLen; i++) {
             // Check the hash values of current window of text
             // and pattern. If the hash values match then only
-            // check for characters on by one if (p == t) {
-            /* Check for characters one by one */
-            for (j = 0; j < M; j++) {
+            for (j = 0; j < patternLen; j++) {
                 if (txt.charAt(i + j) != pat.charAt(j))
                     break;
             }
             // if p == t and pat[0...M-1] = txt[i, i+1,...i+M-1]
-            if (j == M) {
+            if (j == patternLen) {
                 locations.add(i);
                 System.out.println("Pattern found at index " + i);
             }
         }
         // Calculate hash value for next window of text: Remove
         // leading digit, add trailing digit
-        if (i < N - M) {
-            t = (d * (t - txt.charAt(i) * h) + txt.charAt(i + M)) % q;
-            // We might get negative value of t, converting it
-            // to positive
-            if (t < 0)
-                t = (t + q);
+        if (i < textLen - patternLen) {
+            textHash = (alphabets * (textHash - txt.charAt(i) * h) + txt.charAt(i + patternLen)) % q;
+            // We might get negative value of t, converting to positive
+            if (textHash < 0)
+                textHash = (textHash + q);
         }
     }
 
